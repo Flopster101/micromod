61bc0161776881686fe4447950da4c26e0e77925
Flopster101 <nahuelgomez329@gmail.com>
2026-01-13 21:05:11 -0300

    micromod-js: Split audio playback into AudioWorklet
    Better performance.
    
    Also update testplayer.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- micromod-js/AudioPlayer.js
+++ micromod-js/AudioPlayer.js
@@ -1,27 +1,184 @@
-
 function AudioPlayer() {
-	var audioContext = new AudioContext();
-	var scriptProcessor = audioContext.createScriptProcessor( 0, 0, 2 );
+	var AudioCtx = window.AudioContext || window.webkitAudioContext;
+	var audioContext = new AudioCtx();
 	var audioSource = new SineSource( audioContext.sampleRate );
-	var onaudioprocess = function( event ) {
-		var leftBuf = event.outputBuffer.getChannelData( 0 );
-		var rightBuf = event.outputBuffer.getChannelData( 1 );
-		audioSource.getAudio( leftBuf, rightBuf, event.outputBuffer.length );
-	}
+	var playToken = 0;
+	var isPlaying = false;
+	var scriptProcessor = null;
+	var workletNode = null;
+	var usingWorklet = false;
+	var pumpTimer = null;
+	var bufferedFrames = 0;
+	var bufferCapacityFrames = 0;
+	var underrunFrames = 0;
+	var workletInitPromise = null;
+	var chunkSize = 8192;
+	var targetBufferSeconds = 1.00;
+	var maxBufferSeconds = 3.00;
+	var leftWork = new Float32Array( chunkSize );
+	var rightWork = new Float32Array( chunkSize );
+	var txPool = [];
+
+	var ensureScriptProcessor = function() {
+		if( scriptProcessor ) return;
+		scriptProcessor = audioContext.createScriptProcessor( 0, 0, 2 );
+		scriptProcessor.onaudioprocess = function( event ) {
+			var leftBuf = event.outputBuffer.getChannelData( 0 );
+			var rightBuf = event.outputBuffer.getChannelData( 1 );
+			audioSource.getAudio( leftBuf, rightBuf, event.outputBuffer.length );
+		};
+	};
+
+	var ensureWorklet = function() {
+		if( workletInitPromise ) return workletInitPromise;
+		workletInitPromise = ( async function() {
+			if( !audioContext.audioWorklet || !audioContext.audioWorklet.addModule ) {
+				throw "AudioWorklet not supported";
+			}
+			await audioContext.audioWorklet.addModule( "BufferedStereoWorklet.js" );
+			workletNode = new AudioWorkletNode( audioContext, "buffered-stereo", {
+				numberOfInputs: 0,
+				numberOfOutputs: 1,
+				outputChannelCount: [ 2 ]
+			} );
+			workletNode.port.onmessage = function( event ) {
+				var msg = event.data;
+				if( !msg || !msg.type ) return;
+				switch( msg.type ) {
+					case "level":
+						bufferedFrames = msg.frames | 0;
+						bufferCapacityFrames = msg.capacity | 0;
+						underrunFrames = msg.underrunFrames | 0;
+						break;
+					case "recycle":
+						if( msg.buffer ) {
+							txPool.push( msg.buffer );
+						}
+						break;
+				}
+			};
+			usingWorklet = true;
+		} )();
+		return workletInitPromise;
+	};
+
+	var stopPump = function() {
+		if( pumpTimer ) {
+			clearInterval( pumpTimer );
+			pumpTimer = null;
+		}
+	};
+
+	var pumpOnce = function( targetFrames, maxFrames ) {
+		if( !workletNode ) return;
+		// Fill in chunks, but cap work per tick to avoid long stalls.
+		for( var loops = 0; loops < 8; loops++ ) {
+			if( bufferedFrames >= targetFrames ) return;
+			if( bufferedFrames >= maxFrames ) return;
+			if( leftWork.length !== chunkSize ) {
+				leftWork = new Float32Array( chunkSize );
+				rightWork = new Float32Array( chunkSize );
+			}
+			audioSource.getAudio( leftWork, rightWork, chunkSize );
+			var buf = txPool.length > 0 ? txPool.pop() : new ArrayBuffer( chunkSize * 2 * 4 );
+			var interleaved = new Float32Array( buf );
+			for( var i = 0, j = 0; i < chunkSize; i++ ) {
+				interleaved[ j++ ] = leftWork[ i ];
+				interleaved[ j++ ] = rightWork[ i ];
+			}
+			workletNode.port.postMessage( { type: "push", buffer: buf }, [ buf ] );
+			// bufferedFrames is updated by worklet "level" messages.
+		}
+	};
+
+	var startPump = function() {
+		stopPump();
+		if( !workletNode ) return;
+		var targetFrames = ( targetBufferSeconds * audioContext.sampleRate ) | 0;
+		var maxFrames = ( maxBufferSeconds * audioContext.sampleRate ) | 0;
+		if( bufferCapacityFrames > 0 && maxFrames > bufferCapacityFrames ) {
+			maxFrames = bufferCapacityFrames;
+		}
+		pumpTimer = setInterval( function() {
+			pumpOnce( targetFrames, maxFrames );
+		}, 30 );
+	};
+
+	var prefillAndStart = function() {
+		if( !workletNode ) return;
+		var targetFrames = ( targetBufferSeconds * audioContext.sampleRate ) | 0;
+		var maxFrames = ( maxBufferSeconds * audioContext.sampleRate ) | 0;
+		if( bufferCapacityFrames > 0 && maxFrames > bufferCapacityFrames ) {
+			maxFrames = bufferCapacityFrames;
+		}
+		// Prime buffer synchronously so playback doesn't start at 0.
+		pumpOnce( targetFrames, maxFrames );
+		startPump();
+	};
+
 	this.getSamplingRate = function() {
 		return audioContext.sampleRate;
+	}
+	this.getBackend = function() {
+		return usingWorklet ? "worklet" : "scriptprocessor";
+	}
+	this.getBufferedSeconds = function() {
+		return bufferedFrames / audioContext.sampleRate;
+	}
+	this.getUnderrunFrames = function() {
+		return underrunFrames;
+	}
+	this.setBuffering = function( targetSeconds, maxSeconds, chunkFrames ) {
+		if( targetSeconds > 0 ) targetBufferSeconds = targetSeconds;
+		if( maxSeconds > 0 ) maxBufferSeconds = maxSeconds;
+		if( chunkFrames && chunkFrames >= 256 ) {
+			chunkSize = chunkFrames | 0;
+			leftWork = new Float32Array( chunkSize );
+			rightWork = new Float32Array( chunkSize );
+			txPool = [];
+		}
 	}
 	this.setAudioSource = function( audioSrc ) {
 		audioSource = audioSrc;
 	}
 	this.play = function() {
-		scriptProcessor.onaudioprocess = onaudioprocess;
-		scriptProcessor.connect( audioContext.destination );
+		if( isPlaying ) return;
+		isPlaying = true;
+		var token = ++playToken;
+		audioContext.resume();
+		ensureWorklet().then( function() {
+			if( token !== playToken ) return;
+			if( !workletNode ) throw "Worklet init failed";
+			bufferedFrames = 0;
+			underrunFrames = 0;
+			workletNode.port.postMessage( { type: "reset" } );
+			// Start filling before connecting, to avoid initial underruns.
+			prefillAndStart();
+			workletNode.connect( audioContext.destination );
+		} ).catch( function() {
+			if( token !== playToken ) return;
+			// Common reasons: unsupported browser or loading TestPlayer.html from file://
+			usingWorklet = false;
+			ensureScriptProcessor();
+			scriptProcessor.connect( audioContext.destination );
+		} );
 	}
 	this.stop = function() {
-		if( scriptProcessor.onaudioprocess ) {
-			scriptProcessor.disconnect( audioContext.destination );
-			scriptProcessor.onaudioprocess = null;
+		if( !isPlaying ) return;
+		isPlaying = false;
+		playToken++;
+		stopPump();
+		if( workletNode ) {
+			try {
+				workletNode.disconnect( audioContext.destination );
+			} catch( e ) {
+			}
+		}
+		if( scriptProcessor ) {
+			try {
+				scriptProcessor.disconnect( audioContext.destination );
+			} catch( e ) {
+			}
 		}
 	}
 }
